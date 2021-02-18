1df19974d6ac13d4f2b621b06b33a22d78aee7ff
Daniele Castellana <daniele.castellana@di.unipi.it>
2021-02-18 11:28:18 +0100

    First commit. The file are copied from TensorTreeRNN repo.

---
+++ datasets.py
@@ -0,0 +1,19 @@
+from torch.utils.data import Dataset
+
+
+class ListDataset(Dataset):
+
+    def __init__(self, list):
+        self.data = list
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+    def __len__(self):
+        return len(self.data)
+
+
+class ConstValues:
+    UNK = 0
+    NO_ELEMENT = -1
+
